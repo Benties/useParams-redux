c136d06b01fc92bdb199e5941a7aec0837f237e1
Benjamin Thai <ben.thai@aol.com>
2022-10-11 16:57:21 -0400

    added single article display

--- src/components/SingleArticle/index.js
+++ src/components/SingleArticle/index.js
@@ -1,18 +1,20 @@
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import './SingleArticle.css';
 
 const SingleArticle = () => {
+  const {id} = useParams()
+  const articles = useSelector((state) =>  state.articleState.entries)
+  const currArticle = articles.find((article) => article.id === id)
   return (
     <div className='singleArticle'>
-      <h1>Why Am I At Home</h1>
+      <h1>{currArticle.title}</h1>
       <img
-        src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Frobcain%2Ffiles%2F2017%2F10%2FKevin-Home-Alone.jpg'
-        alt='home'
+        src={currArticle.imageUrl}
+        alt={currArticle.imageUrl}
       />
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex esse
-        laboriosam officia accusantium veritatis fugiat exercitationem vero
-        autem nihil aliquid ullam recusandae, quis odit odio voluptates
-        explicabo nobis! Consequuntur, aliquam?
+        {currArticle.body}
       </p>
     </div>
   );
